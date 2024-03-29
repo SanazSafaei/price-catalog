ab4549fee7f5469106979c32f843fccbd198a9a7
sanaz.safaei <sanaz.safaei@digikala.com>
2024-03-29 16:36:35 +0330

    add test for file_manager

---
+++ test_file_manager.py
@@ -0,0 +1,22 @@
+import unittest
+from file_manager import FileManager
+
+class TestFileManager(unittest.TestCase):
+
+    def test_head_lines(self):
+        headline =['source', 'destination', 'source_type', 'destination_type']
+        mapper_file = FileManager('mappings.csv')
+        self.assertEqual(headline, mapper_file.headlines)
+
+    def test_empty_file(self):
+        headline = None
+        file = FileManager('test.csv')
+        self.assertEqual(file.headlines, headline)
+
+    def test_row_data(self):
+        data = {'source':'winter','destination': 'Winter', 'source_type': 'season', 'destination_type': 'season'}
+        mapper_file = FileManager('mappings.csv')
+        self.assertEqual(data, mapper_file.get_row_data())
+
+if __name__ == '__main__':
+    unittest.main()
