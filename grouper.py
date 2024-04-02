7575d5d0815314f7d1b25ebf9d40c956cf29e280
sanaz.safaei <sanaz.safaei@digikala.com>
2024-04-02 16:55:54 +0330

    add grouper file

---
+++ grouper.py
@@ -0,0 +1,61 @@
+
+
+class Grouper():
+    """"classify list of dictionaries to their common {field: values}"""
+
+    def __init__(self, data_list: list) -> None:
+        self.data_list = data_list
+
+    def group_common_field_values(self) -> list:
+        """"classify a list of dictionary to their common {field: values}"""
+
+        common_field_value = {}
+
+        for item in self.data_list:
+
+            if not common_field_value:
+                common_field_value = item.copy()
+
+            else:
+                common_field_value_temp = common_field_value.copy()
+                for filed, value in common_field_value_temp.items():
+                    if item.get(filed) != value:
+                        common_field_value.pop(filed)
+
+        return common_field_value
+
+    def group_by_spesefic_field(self, article_identifier_name: str) -> dict:
+        """"classify a list of dictionary with a identifier and
+        find objects common {article_identifier_name: values}"""
+
+        grouped_items = {}
+        common_field_value = {}
+        deleted_fields = []
+
+        for item in self.data_list:
+
+            if item.get(article_identifier_name) and item.get(article_identifier_name) in grouped_items:
+                grouped_items[item[article_identifier_name]].append(item)
+
+                common_field_value_copy = common_field_value.copy()
+                for item_field, value_lists in common_field_value_copy.items():
+
+                    if ((value_lists.get(item[article_identifier_name]) and 
+                         item[item_field] != value_lists[item[article_identifier_name]]) or
+                         not value_lists.get(item[article_identifier_name])) :
+
+                        deleted_fields.append(item_field)
+                        common_field_value.pop(item_field)
+
+            else:
+                grouped_items[item[article_identifier_name]] = [item]
+                for item_field in item:
+                    if item_field not in deleted_fields:
+
+                        if common_field_value.get(item_field):
+                            common_field_value.get(item_field).update({item[article_identifier_name]: item[item_field]})
+                        else:
+                            val = {item[article_identifier_name]: item[item_field]}
+                            common_field_value[item_field] = val
+
+        return grouped_items, common_field_value
