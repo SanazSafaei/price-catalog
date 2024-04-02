2af9a90f1fb3ecef7c5183f9bf1a8bcf0aefeb2e
sanaz.safaei <sanaz.safaei@digikala.com>
2024-04-02 17:47:12 +0330

    customise value error for better message

--- convert_to_json.py
+++ convert_to_json.py
@@ -11,14 +11,18 @@ class ConverToJson():
             json_file_address: str = DEFAULT_JSON_FILE_ADDRESS):
         """converts  Catlog object to a json object."""
 
-        json_data = {}
-        json_data['Catalog'] = catalog.fields.copy()
+        try:
+            json_data = {}
+            json_data['Catalog'] = catalog.fields.copy()
 
-        json_data['Catalog']['Articles'] = []
-        for article in catalog.articles:
-            # article_data = article.fields.copy()
-            # article_data['Variation'] = article.variations
-            json_data['Catalog']['Articles'].append(article.fields | {'Variations': article.variations})
+            json_data['Catalog']['Articles'] = []
+            for article in catalog.articles:
+                # article_data = article.fields.copy()
+                # article_data['Variation'] = article.variations
+                json_data['Catalog']['Articles'].append(article.fields | {'Variations': article.variations})
 
-        with open(json_file_address+".json", "w", encoding="utf-8") as file:
-            json.dump(json_data, file)
+            with open(json_file_address+".json", "w", encoding="utf-8") as file:
+                json.dump(json_data, file)
+
+        except TypeError:
+            raise TypeError('Catalog is not valid')
