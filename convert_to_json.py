cc15dd7619e98f3e09785557346a10c093f98f84
sanaz.safaei <sanaz.safaei@digikala.com>
2024-04-02 16:55:28 +0330

    store json data in file and fix tags

--- convert_to_json.py
+++ convert_to_json.py
@@ -4,16 +4,21 @@ from catalog import Catalog
 class ConverToJson():
     """converts objects to a json object."""
 
-    def convert_catalog_to_json(self, catalog: Catalog):
+    DEFAULT_JSON_FILE_ADDRESS = 'json_price_catalog'
+
+    def convert_catalog_to_json(
+            self, catalog: Catalog,
+            json_file_address: str = DEFAULT_JSON_FILE_ADDRESS):
         """converts  Catlog object to a json object."""
 
-        json_data = catalog.fields.copy()
+        json_data = {}
+        json_data['Catalog'] = catalog.fields.copy()
 
-        json_data['articles'] = []
+        json_data['Catalog']['Articles'] = []
         for article in catalog.articles:
-            article_data = article.fields.copy()
-            article_data['variations'] = article.variations
-            json_data['articles'].append(article.fields | {'variation': article.variations})
+            # article_data = article.fields.copy()
+            # article_data['Variation'] = article.variations
+            json_data['Catalog']['Articles'].append(article.fields | {'Variations': article.variations})
 
-        with open("json_price_catalog.json", "w", encoding="utf-8") as file:
+        with open(json_file_address+".json", "w", encoding="utf-8") as file:
             json.dump(json_data, file)
