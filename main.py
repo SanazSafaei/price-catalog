09fd16fef0d2905cfb35100350fe2c16c6c5ea46
sanaz.safaei <sanaz.safaei@digikala.com>
2024-04-03 23:11:19 +0330

    add input arguments

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
+from argparse import ArgumentParser
 from price_catalog import PriceCatalog
 from convert_to_json import ConverToJson
 
+class Main():
 
-def main():
+    def main(self):
+        """execution of program"""
 
-    price_cat = PriceCatalog('pricat.csv')
-    catalaog = price_cat.create_catalog('article_number')
-    ConverToJson().convert_catalog_to_json(catalaog)
-    return
+        parser = self.define_input_arguments()
+        input_args = parser.parse_args()
+
+        price_cat = PriceCatalog(input_args.file_path, input_args.mapper_path)
+        catalaog = price_cat.create_catalog(input_args.article_identifier)
+        ConverToJson().convert_catalog_to_json(catalaog)
+
+    def define_input_arguments(self) -> ArgumentParser:
+        """defines inputs and main --help fields"""
+
+        parser = ArgumentParser(description='Process catlog csv data and convert it to json file.')
+        parser.add_argument('-f', '--filename', dest='file_path', type=str, default='pricat.csv',
+                            help='Path of data file. sample /dir/pricat.csv')
+        parser.add_argument('-m','--mapper', dest='mapper_path',type=str, default='mappings.csv',
+                            help='Path of mapping file. sample /dir/pricat.csv')
+        parser.add_argument('-a' ,'--article_identifier', dest='article_identifier',
+                            type=str, default='article_number',
+                            help='Path of mapping file. sample /dir/pricat.csv')
+
+        return parser
+
 
 if __name__ == '__main__':
-    main()
+    Main().main()
